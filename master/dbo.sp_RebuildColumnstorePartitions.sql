540ac3cf04fc87b6ba13c1add629ec565e5695c4
denis.korenic <denis.korenic@gmail.com>
2018-12-17 16:08:43 +0100

    init master.dbo.sp_RebuildColumnstorePartitions

--- master/dbo.sp_RebuildColumnstorePartitions.sql
+++ master/dbo.sp_RebuildColumnstorePartitions.sql
@@ -1,38 +1,37 @@
-USE master;
-GO
-SET ANSI_NULLS ON;
-GO
-SET QUOTED_IDENTIFIER ON;
-GO
-IF OBJECT_ID('dbo.sp_RebuildColumnstorePartitions') IS NULL EXEC('CREATE PROCEDURE dbo.sp_RebuildColumnstorePartitions AS PRINT 1')
+USE master
 GO
 
-ALTER PROCEDURE dbo.sp_RebuildColumnstorePartitions
+
+SET QUOTED_IDENTIFIER ON;
+SET ANSI_NULLS ON;
+GO
+ALTER PROC dbo.sp_RebuildColumnstorePartitions
     @tableName sysname = NULL
   , @steps int = 1
   , @dryRun bit = 0
 AS
-DECLARE @partition_number int
-      , @objectId         int
-      , @indexId          int;
 
-WHILE @steps > 0
-BEGIN
-    SET @steps -= 1;
-
+DECLARE @sql nvarchar(MAX) = '
     SELECT TOP 1
         @partition_number = t.partition_number
       , @objectId         = t.object_id
       , @indexId          = t.index_id
+	  , @indexName		  = i.name
+	  , @partitioned	  = IIF(ps.data_space_id IS NULL, 0, 1)
     FROM sys.dm_db_column_store_row_group_physical_stats AS t
+		JOIN sys.indexes i 
+			ON t.index_id = i.index_id 
+				AND t.object_id = i.object_id
+		LEFT JOIN sys.partition_schemes AS ps
+            ON i.data_space_id = ps.data_space_id
     WHERE (
               t.object_id = OBJECT_ID(@tableName)
-              OR NULLIF(LTRIM(RTRIM(@tableName)), '') IS NULL
+              OR NULLIF(LTRIM(RTRIM(@tableName)), '''') IS NULL
           )
           AND
           (
-              t.state_desc != 'COMPRESSED'
-              OR t.transition_to_compressed_state_desc != 'INDEX_BUILD'
+              t.state_desc != ''COMPRESSED''
+              OR t.transition_to_compressed_state_desc != ''INDEX_BUILD''
               OR t.deleted_rows > 0
           )
     /*
@@ -44,37 +43,36 @@ BEGIN
           )
 		  */
     ORDER BY t.created_time;
-    IF @@ROWCOUNT = 0
+    SET @rc = @@ROWCOUNT;';
+
+WHILE @steps > 0
+BEGIN
+    SET @steps -= 1;
+
+    DECLARE @partition_number int
+          , @objectId         int
+          , @indexId          int
+          , @indexName        sysname
+          , @partitioned      bit
+          , @rc               int = 0;
+
+	--PRINT @sql
+
+	EXEC sys.sp_executesql @sql, N'@tableName sysname, @partition_number int OUT, @objectId int OUT, @indexId int OUT, @indexName sysname OUT, @partitioned bit OUT, @rc int OUT', @tableName, @partition_number OUT, @objectId OUT, @indexId OUT, @indexName OUT, @partitioned OUT, @rc OUT
+
+    IF @rc = 0
         BREAK;
 
-    DECLARE @indexName sysname = (
-                                     SELECT name FROM sys.indexes WHERE index_id = @indexId AND object_id = @objectId
-                                 );
-
-    DECLARE @partitioned bit = (
-                                   SELECT COUNT(1)
-                                   FROM sys.indexes               AS i
-                                       JOIN sys.partition_schemes AS ps
-                                           ON ps.data_space_id = i.data_space_id
-                                              AND i.index_id = @indexId
-                                              AND i.object_id = @objectId
-                               );
-
-
-    DECLARE @sql nvarchar(MAX) = CONCAT('ALTER INDEX ', QUOTENAME(@indexName), ' ON ', QUOTENAME(OBJECT_SCHEMA_NAME(@objectId)), '.', QUOTENAME(OBJECT_NAME(@objectId)), ' REBUILD');
+    DECLARE @sql2 nvarchar(MAX) = CONCAT('ALTER INDEX ', QUOTENAME(@indexName), ' ON ', QUOTENAME(OBJECT_SCHEMA_NAME(@objectId)), '.', QUOTENAME(OBJECT_NAME(@objectId)), ' REBUILD');
     IF @partitioned = 1
-        SET @sql = CONCAT(@sql, ' PARTITION = ', @partition_number);
+        SET @sql2 = CONCAT(@sql2, ' PARTITION = ', @partition_number);
 
-    PRINT @sql;
+    PRINT @sql2;
 
     IF @dryRun = 0
-        EXEC (@sql);
+        EXEC (@sql2);
 
 END;
+
 GO
-
-
-
-
-
 
